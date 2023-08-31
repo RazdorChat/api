82310cb4ab0096e6dea233c2b1b624ee79f0592d
wolfirik <39264658+Serbirial@users.noreply.github.com>
2023-08-30 16:27:56 -0800

    Remove import too...

--- blueprints/group.py
+++ blueprints/group.py
@@ -2,7 +2,6 @@ from sanic import Blueprint
 
 from blueprints.message import blueprint as message
 from blueprints.nodes import blueprint as nodes
-from blueprints.sse import blueprint as sse
 from blueprints.user import blueprint as user
 
 # All of the API
