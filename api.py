8d4c8f028d760bafa11ede1dde003403950385ea
wolfirik <39264658+Serbirial@users.noreply.github.com>
2023-09-10 05:12:51 -0400

    Catch invalid method usage error from sanic.

--- api.py
+++ api.py
@@ -12,7 +12,12 @@ from time import mktime
 from typing import TYPE_CHECKING
 
 from sanic import Sanic
-from sanic.exceptions import NotFound, SanicException, InvalidUsage
+from sanic.exceptions import (
+    NotFound,
+    SanicException,
+    InvalidUsage,
+    MethodNotAllowed
+)
 from sanic.response import HTTPResponse, html
 from sanic.request import Request
 from sanic_ext import Extend
@@ -147,20 +152,23 @@ async def catch_everything(request: Request, exception: Exception) -> HTTPRespon
     # TODO: work on this more, i dont know how sanic errors work and how to isinstance them
     if isinstance(exception, NotFound):
         return HTTPResponse("URL not found.", 404)
+    elif isinstance(exception, MethodNotAllowed):
+        return HTTPResponse("Invalid method usage. Refer to the Documentation for the correct methods to use on endpoints.", 400)
     elif isinstance(exception, InvalidUsage):
-        return HTTPResponse("Invalid usage. Refer to the Documentation for the correct methods to use on URLs, POSTs cannot be sent to GET endpoints, and so on.")
-    _traceback = traceback.extract_tb(exception.__traceback__)
-    to_write = f"exception: {str(exception)}\ntraceback:\n{str(_traceback)}"
-    if _discord_webhooks["enabled"] == True:
-        webhook = discord_legacy_webhook.DiscordWebhook(_discord_webhooks["error"], "API Error")
-        try:
-            webhook.send(str(_traceback), str(exception))
-        except Exception as e:
-            to_write += f"\nwebhook: {str(e)}"  # Probably a bad webhook
-            # TODO: describe error more
-    log_file.write(to_write)
-    logger.error(to_write)
-    return HTTPResponse("Something happened internally, it has been reported and we will fix the error as soon as possible.", 500)
+        return HTTPResponse("Invalid usage. Refer to the Documentation for the correct methods to use on URLs, POSTs cannot be sent to GET endpoints, and so on.", 400)
+    else:
+        _traceback = traceback.extract_tb(exception.__traceback__)
+        to_write = f"exception: {str(exception)}\ntraceback:\n{str(_traceback)}"
+        if _discord_webhooks["enabled"] == True:
+            webhook = discord_legacy_webhook.DiscordWebhook(_discord_webhooks["error"], "API Error")
+            try:
+                webhook.send(str(_traceback), str(exception))
+            except Exception as e:
+                to_write += f"\nwebhook: {str(e)}"  # Probably a bad webhook
+                # TODO: describe error more
+        log_file.write(to_write)
+        logger.error(to_write)
+        return HTTPResponse("Something happened internally, it has been reported and we will fix the error as soon as possible.", 500)
 
 
 # Close the DB on exit
